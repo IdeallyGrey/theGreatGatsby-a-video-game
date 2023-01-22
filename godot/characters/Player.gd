5477662e5fc24f49385a1023c1b3c86338a8cd59
Lukas H <lukas@ideallygrey.uk>
2023-01-22 12:22:41 -0500

    HB 8

--- godot/characters/Player.gd
+++ godot/characters/Player.gd
@@ -1,10 +1,14 @@
 extends KinematicBody2D
 
-const ACCELERATION = 450
-const MAX_SPEED = 35
-const FRICTION = 450
+const ACCELERATION = 350
+const MAX_SPEED = 32
+const FRICTION = 400
 
 var velocity = Vector2.ZERO
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -13,8 +17,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Walk/blend_position", input_vector)
+		animationState.travel("Walk")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 
 	velocity = move_and_slide(velocity)
