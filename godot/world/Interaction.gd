83c161fb90ae4fd1bf7519edd86b5fd052e70526
Lukas H <lukas@ideallygrey.uk>
2023-01-23 22:48:29 -0500

    Comments because its almost 23:00 and I'm tired

--- godot/world/Interaction.gd
+++ godot/world/Interaction.gd
@@ -6,15 +6,13 @@ var interacting = false
 func _ready():
 	pass # Replace with function body.
 
-
-
 func _process(_delta):
 	touchingAreas = get_overlapping_areas()
-	if touchingAreas:
-		if not interacting:
-			if Input.get_action_strength("ui_accept"):
+	if touchingAreas:		# Am I touching something?
+		if not interacting:		# Am I already interacting with it?
+			if Input.get_action_strength("ui_accept"):		# Do I want to interact?
 				print("Interacted!")
 				interacting = true
 		else:
-			if not Input.get_action_strength("ui_accept"):
+			if not Input.get_action_strength("ui_accept"):		# Am I done interacting?
 				interacting = false
